8c30a93a125067d736fe891abeb7c509fb5b9256
Hony <438661443@qq.com>
2016-12-23 11:20:31 +0800

    f

--- MyRx/Pods/Target Support Files/Pods-MyRx/Pods-MyRx-frameworks.sh
+++ MyRx/Pods/Target Support Files/Pods-MyRx/Pods-MyRx-frameworks.sh
@@ -84,8 +84,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CGFloatLiteral/CGFloatLiteral.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ManualLayout/ManualLayout.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Moya/Moya.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReusableKit/ReusableKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxDataSources/RxDataSources.framework"
@@ -95,8 +98,11 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Then/Then.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CGFloatLiteral/CGFloatLiteral.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ManualLayout/ManualLayout.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Moya/Moya.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReusableKit/ReusableKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxDataSources/RxDataSources.framework"
