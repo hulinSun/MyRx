79a5a5661bd681cdf83b872b28e31572bdaaf02a
Hony <438661443@qq.com>
2016-12-27 14:17:35 +0800

    fix

--- MyRx/MyRx/ViewController.swift
+++ MyRx/MyRx/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let param = [
+                "lastid": "0",
+                "source": "APP",
+                "uid": "1248932",
+                "register_id": "",
+                "platform": "IOS",
+                "udid": "e0164119fcedd620533b1a6a163454b13b4be0e9",
+                "user_id": "1248932",
+                "version": "4.9.0",
+                "token_key": "MTI0ODkzMizmiafov7dfLCw2ZGFmMzs5YWMwNmU0OWM0OWY5MTgzNjc0MGVlZTA5Njk5ZDBhYg=="
+            ]
+        
+        Alamofire.request( "https://soa.ihuochaihe.com:442/v1/thread/momentsad", method: .post, parameters: param).responseString { (rsp) in
+            print(rsp)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
+    
+    
+    
 
 }
 
--- MyRx/Pods/Target Support Files/Pods-MyRx/Pods-MyRx-frameworks.sh
+++ MyRx/Pods/Target Support Files/Pods-MyRx/Pods-MyRx-frameworks.sh
@@ -86,6 +86,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CGFloatLiteral/CGFloatLiteral.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ManualLayout/ManualLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Moya/Moya.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ObjectMapper/ObjectMapper.framework"
@@ -102,6 +103,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CGFloatLiteral/CGFloatLiteral.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ManualLayout/ManualLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Moya/Moya.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ObjectMapper/ObjectMapper.framework"
