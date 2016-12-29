7f3b0079188b6d9cbb86dcb693fc74c01e5b5b00
Hony <438661443@qq.com>
2016-12-29 15:15:29 +0800

    fix

---
+++ MyRx/MyRx/Classes/Main/Appdelegate+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Appdelegate+Extension.swift
+//  MyRx
+//
+//  Created by Hony on 2016/12/29.
+//  Copyright © 2016年 Hony. All rights reserved.
+//
+
+import UIKit
+
+extension AppDelegate{
+    
+    func configWindow()  {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = .white
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = MainTabbarController()
+    }
+}
+
---
+++ MyRx/MyRx/Classes/Main/Controller/MainTabbarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabbarController.swift
+//  MyRx
+//
+//  Created by Hony on 2016/12/29.
+//  Copyright © 2016年 Hony. All rights reserved.
+//
+
+import UIKit
+
+class MainTabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
