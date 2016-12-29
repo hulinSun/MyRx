9cbb881c90ab7c7e7cabb18882c4c23383468928
Hony <438661443@qq.com>
2016-12-29 18:35:32 +0800

    fix

---
+++ MyRx/MyRx/Classes/Core/Tool/JSONTool.swift
@@ -0,0 +1,17 @@
+//
+//  JSONTool.swift
+//  MyRx
+//
+//  Created by Hony on 2016/12/29.
+//  Copyright © 2016年 Hony. All rights reserved.
+//
+
+import UIKit
+
+
+class JSONTool: NSObject {
+    
+    class func dataWith(name: String) -> Data{
+    }
+
+}
--- MyRx/MyRx/Classes/Match/Controller/MatchViewController.swift
+++ MyRx/MyRx/Classes/Match/Controller/MatchViewController.swift
@@ -39,5 +39,4 @@ class MatchViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-
 }
