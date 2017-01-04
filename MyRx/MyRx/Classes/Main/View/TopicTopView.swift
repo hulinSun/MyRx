4db2a888ecfd96c61036699ef36ff8908cd8cdb2
Hony <438661443@qq.com>
2017-01-04 20:22:19 +0800

    fix

---
+++ MyRx/MyRx/Classes/Main/View/TopicTopView.swift
@@ -0,0 +1,26 @@
+//
+//  TopicTopView.swift
+//  MyRx
+//
+//  Created by Hony on 2017/1/4.
+//  Copyright © 2017年 Hony. All rights reserved.
+//
+
+import UIKit
+
+class TopicTopView: UIView {
+
+    @IBOutlet weak var namelabel: UILabel!
+
+    @IBOutlet weak var leftBtn: UIButton!
+
+    @IBOutlet weak var iconView: UIImageView!
+    
+    @IBOutlet weak var memoLabel: UILabel!
+    
+    @IBOutlet weak var rightBtn: UIButton!
+
+    func awakeFromNib() {
+        
+    }
+}
