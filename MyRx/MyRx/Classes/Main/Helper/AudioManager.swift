6ec332b7898d780ee14c0e7ca02a3ea5fe980f52
Hony <438661443@qq.com>
2017-01-09 15:55:20 +0800

    fix

---
+++ MyRx/MyRx/Classes/Main/Helper/AudioManager.swift
@@ -0,0 +1,19 @@
+//
+//  AudioManager.swift
+//  MyRx
+//
+//  Created by Hony on 2017/1/9.
+//  Copyright © 2017年 Hony. All rights reserved.
+//
+
+import UIKit
+
+
+class AudioManager {
+    
+    // 单例
+    static let sharedManager = AudioManager()
+    private init(){}
+    
+    
+}
