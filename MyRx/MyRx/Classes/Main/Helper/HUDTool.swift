e97f41ca265ff633b82177625dddbd6357f4a4e9
Hony <438661443@qq.com>
2017-01-13 11:14:35 +0800

    f

--- MyRx/MyRx/Classes/Main/Helper/HUDTool.swift
+++ MyRx/MyRx/Classes/Main/Helper/HUDTool.swift
@@ -10,26 +10,6 @@ import UIKit
 
 
 class HUDTool: NSObject {
-    
-    /// 显示文字
-    class func showText(message: String){
-        HUD.flash(.label(message), delay: 2.0) { _ in
-        }
-    }
-    
-    /// 类似于sv的圈圈
-    class func showProgress(){
-       HUD.flash(.rotatingImage(UIImage(named: "progress")), delay: 2.0)
-    }
-    
-    class func showSuccess(){
-        HUD.flash(.success, delay: 2.0)
-    }
-    
-    class func showError(){
-        HUD.show(.error)
-        HUD.hide(afterDelay: 2.0)
-    }
-    
+        
     
 }
